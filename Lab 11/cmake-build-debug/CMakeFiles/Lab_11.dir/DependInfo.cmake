b9d44185cd78c03de20d2b94a64da72e4d2341d5
Daniel Innes <Daniel.W.Innes@gmail.com>
2019-01-23 09:42:16 -0500

    upload

---
+++ Lab 11/cmake-build-debug/CMakeFiles/Lab_11.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Danie/Documents/University/year 1/term 2/Programming - SYSC2006/Labs/Lab_11/main.c" "C:/Users/Danie/Documents/University/year 1/term 2/Programming - SYSC2006/Labs/Lab_11/cmake-build-debug/CMakeFiles/Lab_11.dir/main.c.obj"
+  "C:/Users/Danie/Documents/University/year 1/term 2/Programming - SYSC2006/Labs/Lab_11/sl_list.c" "C:/Users/Danie/Documents/University/year 1/term 2/Programming - SYSC2006/Labs/Lab_11/cmake-build-debug/CMakeFiles/Lab_11.dir/sl_list.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab 11/cmake-build-debug/CMakeFiles/Lab_11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_11.dir/main.c.obj"
+  "CMakeFiles/Lab_11.dir/sl_list.c.obj"
+  "Lab_11.pdb"
+  "Lab_11.exe"
+  "Lab_11.exe.manifest"
+  "libLab_11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
